63a6729f31040e6c2e993a8fe09d88bc4d551535
sohanmadhana <madhana.sohan@gmail.com>
2021-09-02 12:34:51 +0530

    updated database values in settings.py

--- planner/settings.py
+++ planner/settings.py
@@ -86,10 +86,10 @@ DATABASES = {
     # 
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'plannerdb',
-        'USER': 'postgres',
-        'PASSWORD': 'planner2021',
-        'HOST': 'tasktodoapp.cpvuapjpbuz2.us-west-1.rds.amazonaws.com',
+        'NAME': os.environ.get('NAME'),
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': os.environ.get('HOST'),
         'PORT': '5432'
     }
         
